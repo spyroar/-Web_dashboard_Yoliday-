750c8f861babb9252018cf1959c286ff65f4fef5
Shivpal <yadavshivpal1999@gmail.com>
2024-12-25 12:28:27 +0530

    add env config

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
-const port = 3000
+require('dotenv').config()
+const port = process.env.PORT||3001
 const router = require('./routes/route')
 
 const cors=require('cors');
